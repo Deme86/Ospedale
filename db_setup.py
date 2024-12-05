bebba9b3d4677f60ec9252590673e9018f268bde
Deme86 <demetrio.scuncia@gmail.com>
2024-12-05 20:06:53 +0100

    Update db_setup.py
    Questo script crea le tabelle del database.

--- db_setup.py
+++ db_setup.py
@@ -1,1 +1,11 @@
+from sqlalchemy import create_engine
+from models import Base
+from config.config import DATABASE_URI
 
+def setup_database():
+    engine = create_engine(DATABASE_URI)
+    Base.metadata.create_all(engine)
+    print("Tabelle create con successo!")
+
+if __name__ == "__main__":
+    setup_database()
